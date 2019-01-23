4caed392a039dcbdbf10b5e78e351e9c2849affd
Naveen Pete <naveen.shenoy.blr@gmail.com>
2019-01-23 11:45:30 +0530

    commit code to repo

--- apps/day-03/store-app/src/app/products/products.component.ts
+++ apps/day-03/store-app/src/app/products/products.component.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 
+import { Product } from '../models/product';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  productId: number = 1;
-  productName: string = 'Dell Latitude';
-  productDescription: string = 'Best for business use';
-  productPrice: number = 45000;
-  productIsAvailable: boolean = false;
+  showMessage: boolean = false;
+  product: Product = new Product();
+
+  products: Product[] = [
+    {
+      id: 1,
+      name: 'Dell Latitude',
+      description: 'Best for business use',
+      price: 45000,
+      isAvailable: false
+    },
+    {
+      id: 2,
+      name: 'Dell XPS',
+      description: 'Premium laptop',
+      price: 95000,
+      isAvailable: true
+    },
+    {
+      id: 3,
+      name: 'Dell Inspiron',
+      description: 'Best for home use',
+      price: 60000,
+      isAvailable: true
+    }
+  ];
+
+  onSubmit() {
+    console.log('form submitted');
+    console.log(this.product);
+    this.products.unshift(this.product);
+    this.product = new Product();
+
+    this.showMessage = true;
+  }
 }
