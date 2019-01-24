e7bf7ae50a484a39c184b1f4f20ee091acaea617
Naveen Pete <naveen.shenoy.blr@gmail.com>
2019-01-24 10:04:23 +0530

    commit changes to rxjs sample code

--- apps/day-04/rxjs-examples/src/app.js
+++ apps/day-04/rxjs-examples/src/app.js
@@ -1,28 +1,13 @@
 import $ from 'jquery';
 
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/fromPromise';
-import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/fromEvent';
 
-const getUser = id => {
-  return $.get(`https://jsonplaceholder.typicode.com/users/${id}`).promise();
-};
+const ctrl = $('#btn');
 
-const promise = getUser(1);
-
-const stream = Observable.fromPromise(promise).map(user => {
-  const newUser = {
-    name: user.name,
-    phone: user.phone,
-    company: user.company.name,
-    city: user.address.city
-  };
-
-  return newUser;
-});
-
+const stream = Observable.fromEvent(ctrl, 'click');
 stream.subscribe(
-  user => console.log('user:', user),
+  event => console.log('event:', event),
   err => console.log('error:', err),
   () => console.log('completed!')
 );
